9df916b9ba450f7ac2544ca8e1ada93ae08e78a2
35917560 <zw19860913@gmail.com>
2017-08-24 02:30:21 +0800

    CDT

--- truffle.js
+++ truffle.js
@@ -10,7 +10,8 @@ module.exports = {
     development: {
       host: "localhost",
       port: 8545,
-      network_id: "*" // Match any network id
+      network_id: "*" ,// Match any network id
+      gasPrice:40000000000
     }
   }
 };
