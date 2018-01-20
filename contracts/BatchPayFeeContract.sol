d5c089d222ac784145ecde6284b4d77c122374e3
zhengwei <>
2018-01-20 09:20:00 +0800

    修改bug

--- contracts/BatchPayFeeContract.sol
+++ contracts/BatchPayFeeContract.sol
@@ -1,6 +1,8 @@
 pragma solidity ^0.4.13;
 
 import 'zeppelin-solidity/contracts/ownership/Ownable.sol';
+import 'zeppelin-solidity/contracts/math/SafeMath.sol';
+import 'zeppelin-solidity/contracts/ReentrancyGuard.sol';
 contract BatchPayFeeContract is Ownable,ReentrancyGuard{
 	using SafeMath for uint256;
 
