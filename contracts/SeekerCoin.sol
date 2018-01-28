9f2f80788d835a89057f5aaa63f3aa470bcd9bd3
Jim Green <zw19860913@163.com>
2018-01-28 10:42:55 +0800

    seekerCoin

---
+++ contracts/SeekerCoin.sol
@@ -0,0 +1,150 @@
+pragma solidity ^0.4.13;
+
+import 'zeppelin-solidity/contracts/token/ERC20.sol';
+import 'zeppelin-solidity/contracts/ownership/Ownable.sol';
+import 'zeppelin-solidity/contracts/math/SafeMath.sol';
+contract SeekerCoin is ERC20,Ownable{
+	using SafeMath for uint256;
+
+	//the base info of the token
+	string public constant name="Seeker Coin";
+	string public symbol="待定";
+	string public constant version = "1.0";
+	uint256 public constant decimals = 18;
+
+	uint256 public rate;
+	uint256 public totalFundingSupply;
+	//the max supply
+	uint256 public MAX_SUPPLY;
+
+	//user's locked balance
+	mapping(address=>epoch[]) public lockEpochsMap;
+
+    mapping(address => uint256) balances;
+	mapping (address => mapping (address => uint256)) allowed;
+	struct epoch  {
+        uint256 endTime;
+        uint256 amount;
+    }
+
+	function SeekerCoin(){
+		MAX_SUPPLY = 10000000000*10**decimals;
+		rate = 0;
+		totalFundingSupply = 0;
+		totalSupply = 0;
+	}
+
+	modifier notReachTotalSupply(uint256 _value,uint256 _rate){
+		assert(MAX_SUPPLY>=totalSupply.add(_value.mul(_rate)));
+		_;
+	}
+
+	function lockBalance(address user, uint256 amount,uint256 endTime) external
+		onlyOwner
+	{
+		 epoch[] storage epochs = lockEpochsMap[user];
+		 epochs.push(epoch(endTime,amount));
+	}
+	
+	function () payable external
+	{
+			processFunding(msg.sender,msg.value,rate);
+			uint256 amount=msg.value.mul(rate);
+			totalFundingSupply = totalFundingSupply.add(amount);
+	}
+
+	function processFunding(address receiver,uint256 _value,uint256 _rate) internal
+		notReachTotalSupply(_value,_rate)
+	{
+		uint256 amount=_value.mul(_rate);
+		totalSupply=totalSupply.add(amount);
+		balances[receiver] +=amount;
+		Transfer(0x0, receiver, amount);
+	}
+
+    function withdrawCoinToOwner(uint256 _value) external
+		onlyOwner
+	{
+		processFunding(msg.sender,_value,1);
+	}
+	
+	function etherProceeds() external
+		onlyOwner
+
+	{
+		if(!msg.sender.send(this.balance)) revert();
+	}
+
+
+
+	function setRate(uint256 _rate) external
+		onlyOwner
+	{
+		rate=_rate;
+	}
+
+  	function transfer(address _to, uint256 _value) public  returns (bool)
+ 	{
+		require(_to != address(0));
+		epoch[] epochs = lockEpochsMap[msg.sender];
+		uint256 needLockBalance = 0;
+		for(uint256 i;i<epochs.length;i++)
+		{
+			if( now < epochs[i].endTime )
+			{
+				needLockBalance=needLockBalance.add(epochs[i].amount);
+			}
+		}
+
+		require(balances[msg.sender].sub(_value)>=needLockBalance);
+		// SafeMath.sub will throw if there is not enough balance.
+		balances[msg.sender] = balances[msg.sender].sub(_value);
+		balances[_to] = balances[_to].add(_value);
+		Transfer(msg.sender, _to, _value);
+		return true;
+  	}
+
+  	function balanceOf(address _owner) public constant returns (uint256 balance) 
+  	{
+		return balances[_owner];
+  	}
+
+
+  	function transferFrom(address _from, address _to, uint256 _value) public returns (bool) 
+  	{
+		require(_to != address(0));
+
+		epoch[] epochs = lockEpochsMap[_from];
+		uint256 needLockBalance = 0;
+		for(uint256 i;i<epochs.length;i++)
+		{
+			if( now < epochs[i].endTime )
+			{
+				needLockBalance = needLockBalance.add(epochs[i].amount);
+			}
+		}
+
+		require(balances[_from].sub(_value)>=needLockBalance);
+		uint256 _allowance = allowed[_from][msg.sender];
+
+		balances[_from] = balances[_from].sub(_value);
+		balances[_to] = balances[_to].add(_value);
+		allowed[_from][msg.sender] = _allowance.sub(_value);
+		Transfer(_from, _to, _value);
+		return true;
+  	}
+
+  	function approve(address _spender, uint256 _value) public returns (bool) 
+  	{
+		allowed[msg.sender][_spender] = _value;
+		Approval(msg.sender, _spender, _value);
+		return true;
+  	}
+
+  	function allowance(address _owner, address _spender) public constant returns (uint256 remaining) 
+  	{
+		return allowed[_owner][_spender];
+  	}
+
+	  
+}
